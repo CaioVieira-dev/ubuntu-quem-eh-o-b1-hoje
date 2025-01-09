40167b82a38044fcdf1beb1dc397a8573a213dc4
Caio Vieira <caiocesarav0208@gmail.com>
2025-01-09 01:14:49 +0000

    chore: adiciona migração

---
+++ quem-eh-o-b1-hoje/drizzle/0001_lean_ken_ellis.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "quem-eh-o-b1-hoje_click_up_user" ADD COLUMN "can_be_b1" boolean;--> statement-breakpoint
+ALTER TABLE "quem-eh-o-b1-hoje_click_up_user" ADD COLUMN "can_be_b2" boolean;--> statement-breakpoint
+ALTER TABLE "quem-eh-o-b1-hoje_click_up_user" ADD COLUMN "is_listed" boolean;
